6243c459ee8763a2c85d32e07812feca5446eb7d
coding-seal <rodion2023@mail.ru>
2024-11-20 21:56:27 +0300

    rough sketch of code

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,3 +3,4 @@ package domain
 import "errors"
 
 var ErrEmptyQueue = errors.New("no patients on bench")
+var ErrAllDoctorsBusy = errors.New("no available doctors")
---
+++ internal/nurse/nurse.go
@@ -0,0 +1,124 @@
+package nurse
+
+import (
+	"context"
+
+	"github.com/Coding-Seal/arch-model/internal/domain"
+)
+
+type PatientGetter interface {
+	NextPatientID() (int, error)
+}
+
+type Nurse struct {
+	PatentGoneCh          <-chan domain.Event
+	PatentInQueueCh       <-chan domain.Event
+	AppointmentFinishedCh <-chan domain.Event
+
+	doctors        []*doctor
+	previousDoctor int
+
+	patientGetter PatientGetter
+
+	numPatients int32
+}
+
+type doctor struct {
+	ch   chan<- int
+	busy bool
+}
+
+func (n *Nurse) RegisterDoctor() (<-chan int, int) {
+	ch := make(chan int)
+	doctorID := len(n.doctors)
+	n.doctors = append(n.doctors, &doctor{ch: ch, busy: false})
+
+	return ch, doctorID
+}
+
+func (n *Nurse) FindAvailableDoctor() (*doctor, error) {
+	var i = (n.previousDoctor + 1) % len(n.doctors)
+	for ; i != n.previousDoctor; i = (i + 1) % len(n.doctors) {
+		if !n.doctors[i].busy {
+			n.previousDoctor = i
+			return n.doctors[i], nil
+		}
+	}
+
+	if !n.doctors[i].busy {
+		n.previousDoctor = i
+		return n.doctors[i], nil
+	}
+
+	return nil, domain.ErrAllDoctorsBusy
+}
+
+func (n *Nurse) HandlePatientGoneEvent() {
+	n.numPatients--
+}
+
+func (n *Nurse) HandlePatientInQueueEvent() {
+	n.numPatients++
+
+	err := n.sendPatientToDoctor()
+	if err != nil {
+		// TODO: Log error
+	}
+}
+
+func (n *Nurse) HandleAppointmentFinishedEvent(event domain.Event) {
+	e, ok := event.(domain.AppointmentFinishedEvent)
+	if !ok {
+		// TODO: Log error
+	}
+
+	if e.DoctorID >= len(n.doctors) {
+		// TODO: Log error
+	}
+
+	n.doctors[e.DoctorID].busy = false
+
+	if n.numPatients <= 0 {
+		return
+	}
+
+	err := n.sendPatientToDoctor()
+	if err != nil {
+		// TODO: Log error
+	}
+}
+
+func (n *Nurse) sendPatientToDoctor() error {
+	doc, err := n.FindAvailableDoctor()
+	if err != nil {
+		return err
+	}
+
+	patientID, err := n.patientGetter.NextPatientID()
+	if err != nil {
+		return err
+	}
+
+	n.numPatients--
+	doc.ch <- patientID
+	doc.busy = true
+
+	return nil
+}
+
+func (n *Nurse) Run(ctx context.Context) {
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-n.PatentInQueueCh:
+				n.HandlePatientInQueueEvent()
+			case <-n.PatentGoneCh:
+				n.HandlePatientGoneEvent()
+			case event := <-n.PatentInQueueCh:
+				n.HandleAppointmentFinishedEvent(event)
+			}
+		}
+	}()
+}
